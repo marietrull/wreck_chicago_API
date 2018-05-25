2b4a74be24c743ffa3df8a722c39601290590251
marietrull <marietrull@gmail.com>
2018-05-25 15:07:24 -0500

    Post route in UserWreck Controller to add wrecks to the logged in user's list

--- controllers/UserWreckController.rb
+++ controllers/UserWreckController.rb
@@ -16,12 +16,25 @@ class UserWreckController < ApplicationController
 
     #Find the user
       user = User.find_by :username => session[:username]
-      userID = user.id
     #Find all user_wrecks with a matching id
       userWrecks = UserWreck.where(user_id: user.id)
     #return json
       userWrecks.to_json
 
 	end
+
+  # Post route to add a wreck to the DB
+
+  post '/' do
+
+    user = User.find_by :username => session[:username]
+    newUserWreck = UserWreck.create user_id: user.id, wreck_id: @payload[:wreck_id] 
+    {
+      success: true,
+      newUserWreck: newUserWreck,
+      message: "Added new wreck to User's list."
+    }.to_json
+
+  end
 
 end
